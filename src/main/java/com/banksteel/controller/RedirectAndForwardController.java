553ade8ac9690bbab2b362f365de5154ebbfb08a
wukaifeng <1076208637@qq.com>
2019-08-22 10:31:29 +0800

    ssss

--- src/main/java/com/banksteel/controller/RedirectAndForwardController.java
+++ src/main/java/com/banksteel/controller/RedirectAndForwardController.java
@@ -6,25 +6,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RedirectAndForwardController {
-    
-    @RequestMapping(value= {"test1"}, method=RequestMethod.GET)
+
+    @RequestMapping(value = {"test1"}, method = RequestMethod.GET)
     public String test1() {
         System.out.println("=================test1");
-        
+
         return "test1";
     }
-    
-    @RequestMapping(value="test2")
+
+    @RequestMapping(value = "test2")
     public String test2() {
         System.out.println("================test2");
-        
+
         return "redirect:test1";
     }
-    
-    @RequestMapping(value="test3")
+
+    @RequestMapping(value = "test3")
     public String test3() {
         System.out.println("================test3");
-        
+
         return "forward:test2";
     }
 
