2534a363259907b379896513c466accc2ec93add
wukaifeng <1076208637@qq.com>
2019-08-22 10:27:23 +0800

    测试转发和重定向

---
+++ src/main/java/com/banksteel/controller/RedirectAndForwardController.java
@@ -0,0 +1,31 @@
+package com.banksteel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class RedirectAndForwardController {
+    
+    @RequestMapping(value= {"test1"}, method=RequestMethod.GET)
+    public String test1() {
+        System.out.println("=================test1");
+        
+        return "test1";
+    }
+    
+    @RequestMapping(value="test2")
+    public String test2() {
+        System.out.println("================test2");
+        
+        return "redirect:test1";
+    }
+    
+    @RequestMapping(value="test3")
+    public String test3() {
+        System.out.println("================test3");
+        
+        return "forward:test2";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/test1.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@page isELIgnored="false"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>转发&重定向</title>
+</head>
+<body>
+   测试转发和重定向
+</body>
+</html>
